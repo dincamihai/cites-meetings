757b076b74570de265e5e76f64f52a57eedc858c
Alex Morega <alex@grep.ro>
2012-02-20 15:49:49 +0200

    labels for schema fields

--- schema.py
+++ schema.py
@@ -4,12 +4,26 @@ import flatland as fl
 countries = ['it', 'no']
 
 
-person = fl.Dict.of(
-    fl.String.named('name_title').using(optional=True),
-    fl.String.named('first_name'),
-    fl.String.named('last_name'),
-    fl.String.named('address').using(optional=True),
-    fl.Enum.named('country').valued(*countries),
-    fl.String.named('phone').using(optional=True),
-    fl.Boolean.named('invitation'),
+Person = fl.Dict.of(
+    fl.String.named('name_title').using(optional=True) \
+        .with_properties(label=u"Title"),
+
+    fl.String.named('first_name') \
+        .with_properties(label=u"First name"),
+
+    fl.String.named('last_name') \
+        .with_properties(label=u"Last name"),
+
+    fl.String.named('address').using(optional=True) \
+        .with_properties(label=u"Address",
+                         widget='textarea'),
+
+    fl.Enum.named('country').valued(*countries) \
+        .with_properties(label=u"Country"),
+
+    fl.String.named('phone').using(optional=True) \
+        .with_properties(label=u"Telephone"),
+
+    fl.Boolean.named('invitation') \
+        .with_properties(label=u"Invitation"),
 )
