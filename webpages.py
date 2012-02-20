35b9f33fed0d92fd9a14fe82116b7245a2030d09
Alex Morega <alex@grep.ro>
2012-02-20 15:50:19 +0200

    display form

--- webpages.py
+++ webpages.py
@@ -1,4 +1,6 @@
 import flask
+import flatland.out.markup
+import schema
 
 
 webpages = flask.Blueprint('webpages', __name__)
@@ -7,3 +9,30 @@ webpages = flask.Blueprint('webpages', __name__)
 @webpages.route('/')
 def home():
     return "hello world"
+
+
+@webpages.route('/edit')
+def edit():
+    app = flask.current_app
+    return flask.render_template('edit.html', **{
+        'mk': MarkupGenerator(app.jinja_env.get_template('widgets.html')),
+        'person': schema.Person(),
+    })
+
+
+class MarkupGenerator(flatland.out.markup.Generator):
+
+    def __init__(self, template):
+        super(MarkupGenerator, self).__init__('html')
+        self.template = template
+
+    def children_order(self, field):
+        if isinstance(field, flatland.Mapping):
+            return [kid.name for kid in field.field_schema]
+        else:
+            return []
+
+    def widget(self, element):
+        widget_name = element.properties.get('widget', 'input')
+        widget_macro = getattr(self.template.module, widget_name)
+        return widget_macro(self, element)
