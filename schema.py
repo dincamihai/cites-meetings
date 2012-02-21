13749c4fe16e94d974f0d3f341f5eddf48c95c5d
Dragos Catarahia <dragos.catarahia@gmail.com>
2012-02-21 13:45:54 +0200

    replace " with '

--- schema.py
+++ schema.py
@@ -16,27 +16,27 @@ def valid_enum(element, state):
 countries = _load_json("data/countries.json")
 
 Person = fl.Dict.of(
-    fl.String.named('name_title').using(optional=True) \
+    fl.String.named("name_title").using(optional=True) \
         .with_properties(label=u"Personal title"),
 
-    fl.String.named('first_name') \
+    fl.String.named("first_name") \
         .with_properties(label=u"First name"),
 
-    fl.String.named('last_name') \
+    fl.String.named("last_name") \
         .with_properties(label=u"Last name"),
 
-    fl.String.named('address').using(optional=True) \
+    fl.String.named("address").using(optional=True) \
         .with_properties(label=u"Address",
-                         widget='textarea'),
+                         widget="textarea"),
 
     fl.Enum.named("country").valued(*sorted(countries.keys())) \
         .using(validators=[valid_enum]) \
         .with_properties(label=u"Country", widget="select",
                          value_labels=countries),
 
-    fl.String.named('phone').using(optional=True) \
+    fl.String.named("phone").using(optional=True) \
         .with_properties(label=u"Telephone"),
 
-    fl.Boolean.named('invitation') \
+    fl.Boolean.named("invitation") \
         .with_properties(label=u"Invitation"),
 )
