7c59bdb61f0753c1c066b0c9698d2db729f6ffbb
Alex Morega <alex@grep.ro>
2012-02-22 12:32:09 +0200

    use flatland validators for email and date

--- schema.py
+++ schema.py
@@ -1,17 +1,8 @@
 import flatland as fl
+from flatland.validation import IsEmail, Converted
 import os
 import json
 import re
-
-# django regex for email validation
-email_re = re.compile(
-    r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
-    # quoted-string, see also http://tools.ietf.org/html/rfc2822#section-3.2.5
-    r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*"'
-    r')@((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$)'  # domain
-    r'|\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$',
-    re.IGNORECASE
-)
 
 date_re = re.compile(
     r"^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$"
@@ -26,17 +17,6 @@ def valid_enum(element, state):
         return False
     return True
 
-def valid_email(element, state):
-    if not email_re.search(element.value):
-        element.add_error(u"Email is not valid.")
-        return False
-    return True
-
-def valid_date(element, state):
-    if not date_re.search(element.value):
-        element.add_error(u"Not a valid date")
-        return False
-    return True
 
 countries = _load_json("refdata/countries.json")
 languages = _load_json("refdata/languages.json")
@@ -69,7 +49,7 @@ Person = fl.Dict.of(
             .with_properties(widget="textarea"),
 
         CommonString.named("email") \
-            .using(optional=False, label=u"Email", validators=[valid_email]),
+            .using(optional=False, label=u"Email", validators=[IsEmail()]),
 
         CommonString.named("phone").using(label=u"Phone"),
         CommonString.named("cellular").using(label=u"Cellular"),
@@ -114,8 +94,10 @@ Person = fl.Dict.of(
         CommonString.named("more_info").using(optional=True, label=u"More Info"),
         CommonBoolean.named("web_alert").using(label=u"Web alert"),
         CommonBoolean.named("verified").using(label=u"Verified"),
-        CommonString.named("date") \
-            .using(label=u"Date acknowledge", optional=True, validators=[valid_date]),
+        fl.Date.named("date") \
+            .using(label=u"Date acknowledge",
+                   optional=True,
+                   validators=[Converted(incorrect=u"%(label)s is not a valid date")]),
         CommonBoolean.named("attended").using(label=u"Attended"),
     ).using(label=u"More info")
 
