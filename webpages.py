d810b55bc8d4a6f4546cf7ee2acc274b2783e139
Dragos Catarahia <dragos.catarahia@gmail.com>
2012-02-23 11:42:44 +0200

    replace ' with "

--- webpages.py
+++ webpages.py
@@ -3,17 +3,17 @@ import flatland.out.markup
 import schema
 import database
 
-webpages = flask.Blueprint('webpages', __name__)
+webpages = flask.Blueprint("webpages", __name__)
 
-@webpages.route('/')
+@webpages.route("/")
 def home():
-    return flask.render_template('home.html', **{
-        'people': [flask.json.loads(r.data)
+    return flask.render_template("home.html", **{
+        "people": [flask.json.loads(r.data)
                    for r in database.Person.query.all()],
     })
 
-@webpages.route('/new', methods=['GET', 'POST'])
-@webpages.route('/edit/<int:person_id>', methods=['GET', 'POST'])
+@webpages.route("/new", methods=["GET", "POST"])
+@webpages.route("/edit/<int:person_id>", methods=["GET", "POST"])
 def edit(person_id=None):
     app = flask.current_app
 
@@ -22,7 +22,7 @@ def edit(person_id=None):
     else:
         person_row = database.Person.query.get_or_404(person_id)
 
-    if flask.request.method == 'POST':
+    if flask.request.method == "POST":
         form_data = dict(schema.Person.from_defaults().flatten())
         form_data.update(flask.request.form.to_dict())
         person = schema.Person.from_flat(form_data)
@@ -34,12 +34,12 @@ def edit(person_id=None):
             person_row.data = flask.json.dumps(dict(person.flatten()))
             session.add(person_row)
             session.commit()
-            flask.flash("Person information saved", 'success')
-            edit_url = flask.url_for('webpages.edit', person_id=person_row.id)
+            flask.flash("Person information saved", "success")
+            edit_url = flask.url_for("webpages.edit", person_id=person_row.id)
             return flask.redirect(edit_url)
 
         else:
-            flask.flash(u"Errors in person information", 'error')
+            flask.flash(u"Errors in person information", "error")
 
     else:
         if person_row is None:
@@ -47,15 +47,15 @@ def edit(person_id=None):
         else:
             person = schema.Person.from_flat(flask.json.loads(person_row.data))
 
-    return flask.render_template('edit.html', **{
-        'mk': MarkupGenerator(app.jinja_env.get_template('widgets.html')),
-        'person': person,
+    return flask.render_template("edit.html", **{
+        "mk": MarkupGenerator(app.jinja_env.get_template("widgets.html")),
+        "person": person,
     })
 
 class MarkupGenerator(flatland.out.markup.Generator):
 
     def __init__(self, template):
-        super(MarkupGenerator, self).__init__('html')
+        super(MarkupGenerator, self).__init__("html")
         self.template = template
 
     def children_order(self, field):
@@ -66,6 +66,6 @@ class MarkupGenerator(flatland.out.markup.Generator):
 
     def widget(self, element, widget_name=None):
         if widget_name is None:
-            widget_name = element.properties.get('widget', 'input')
+            widget_name = element.properties.get("widget", "input")
         widget_macro = getattr(self.template.module, widget_name)
         return widget_macro(self, element)
